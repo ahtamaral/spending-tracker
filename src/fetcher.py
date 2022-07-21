9c77094ddace5ac2be75bf7e8fc4a3263653fa1f
Htutu87 <artur.ht.amaral@gmail.com>
2022-07-21 17:34:30 -0300

    First version of fetcher working.

---
+++ src/fetcher.py
@@ -0,0 +1,101 @@
+''' Fetcher logic:
+
+* Checa se existe o arquivo "../data/cert.p12"
+    Se não existe, invoca o processo de criá-lo.
+    Se existe, usa-o como parametro para fazer o fetch dos dados
+* Checa data do último fetch.
+    Se arquivo não existe, considera que nunca foi feito um fetch.
+    Se fetch foi feito a menos de 2 dias, impede a operação.
+    Se não, realiza o fetch e disponibiliza os dados na pasta data.
+
+'''
+
+from pynubank import Nubank, MockHttpClient
+import datetime, os, getpass
+
+def fetch():
+
+    print("Calling fetch().")
+
+    # ---
+    # Checking for p12 certificate
+
+    print("Searching for p12 certificate as ../data/cert.p12...")
+
+    if (os.path.exists("../data/cert.p12")):
+        print("Found certificate!")
+    else:
+        print("Certificate not found. Please generate one thoroug pynubank CLI.")
+
+    # ---
+
+
+    # ---
+    # Checking last fetch date.
+    print("reading file ../data/lastFetch.txt")
+    f = open("../data/lastFetch.txt", "r")
+    lastFetchStr = f.read()
+    f.close()
+    print("Last Fetch: " + lastFetchStr)
+
+    date = lastFetchStr.split('-')                      
+    lastFetch = datetime.date(int(date[0]),int(date[1]),int(date[2]))
+    today = datetime.date.today()
+    timeDelta = today - lastFetch 
+
+    if timeDelta.days < 2:
+        print("The last fetch was performed less than 2 days ago.")
+        exit("Very recurring accesses to your account can block it for 72 hours. We will not proceed for safety.")
+
+    # ---
+
+    validCpf = False
+
+    while not validCpf:
+        print("Enter you CPF (Just numbers): ", end="")
+        cpf = input()
+        if len(cpf) != 11:
+            print("Invalid CPF length. Try again.")
+        else:
+            if not cpf.isnumeric():
+                print("Non numeric CPF. Try again.")
+            else:
+                print("Valid CPF.")
+                validCpf = True
+
+    nu_pass = getpass.getpass("Insira sua senha: ")
+
+    # Fetching Nubank server.
+
+    nu = Nubank(MockHttpClient())
+    nu.authenticate_with_cert(cpf, nu_pass, "../data/cert.p12")
+
+    f = open("lastFetch.txt", "w")
+    # Descomentar linha de baixo quando for utilizar pra valer.
+    #f.write(str(today))
+    f.write(str(today-datetime.timedelta(days=7))) # last fetch setado para 7 dias atrás. Para debug.
+    f.close()
+
+    account_feed = nu.get_account_feed()
+    card_statements = nu.get_card_statements()
+
+    account_feed_filename = "../data/" + str(today) + "/account_feed"
+    card_statements_filename = "../data/" + str(today) + "/card_statements"
+
+    os.system("mkdir -p " + account_feed_filename)
+    os.system("mkdir -p " + card_statements_filename)
+
+    account_feed_filename += "/raw.json"
+    card_statements_filename += "/raw.json"
+
+    os.system("touch " + account_feed_filename)
+    os.system("touch " + card_statements_filename)
+
+    print(account_feed)
+    print(card_statements)
+
+    print(account_feed_filename)
+    print(card_statements_filename)
+
+    os.system("echo " + str(account_feed) + " > " + account_feed_filename)
+    os.system("echo " + str(card_statements) + " > " + card_statements_filename)
--- src/main.py
+++ src/main.py
@@ -1,83 +1,14 @@
-import gspread
-import datetime
-import transactions as t
-import utils
 
-# Carrega as transações nas duas listas.
-# Cria uma lista do mês desejado -> Por exemplo, 06, junta os gastos de débito e crédito em uma lista só.
-# Ordena os gastos em ordem crescente de dia.
-# Cria uma worksheet do mês e escreve os dados formatados nela.
+import fetcher as fetcher
 
-card_statements_filename = "../data/card_statements_18-06-2022/raw.json"
-account_feed_filename = "../data/account_feed_18-06-2022/raw.json"
-myTransactions = t.getMyTransactions(card_statements_filename, account_feed_filename);
+# Global variables
+version = 0.1
 
+if __name__ == "__main__":
 
-sa = gspread.service_account()
-sh = sa.open("Pynubank")
+    print("-----------------------\n Nubank Spending Tracker v{}".format(version))
+    print("\n\tAutomatically monitor your credit and debit card spending on your Nubank account.")
+    print("\tAuthor: Artur Amaral\n-----------------------")
 
-yearList = [2020, 2021, 2022]
-month = 5
+    fetcher.fetch()
 
-# Creating all worksheets
-for year in yearList:
-    for month in range(1,13):
-        currentMonth = datetime.date(year,month,1).month
-        currentYear = datetime.date(year,month,1).year
-        currentMonthName = utils.getMonthString(month)
-        wksTitle = str(year) + "/" + currentMonthName
-        try:
-            1+1
-            #wks = sh.add_worksheet(title=wksTitle, rows="200", cols="20")
-        except:
-            print("Worksheet already exists.")
-            #wks = sh.worksheet(wksTitle)
-        print(wksTitle)
-
-# Ordering transactions by month
-        monthlyTransactions = []
-
-        for i in myTransactions["credit"]:
-            if (i.date.year == currentYear) and (i.date.month == currentMonth):
-                monthlyTransactions += [i]
-
-        for i in myTransactions["debit"]:
-            if (i.date.year == currentYear) and (i.date.month == currentMonth):
-                monthlyTransactions += [i]
-
-        monthlyTransactions.sort(key = lambda x: x.date, reverse=False)
-
-        spreadData = []
-        transactionData = []
-
-        for i in monthlyTransactions:
-            if str(type(i)) == "<class 'transactions.CreditTransaction'>":
-                #print("[CREDITO] " + str(i.date))
-                transactionData = [i.description, "Crédito",  i.amount, str(i.date)]
-            else:
-                #print("[DEBITO] " + str(i.date))
-                transactionData = ["[" + i.typename + "] " + i.title, "Débito", i.detail, str(i.date)]
-                spreadData += [transactionData]
-            print(transactionData)  
-        nRows = len(spreadData)
-        spreadRange = "A1:D" + str(nRows)
-        print(spreadRange)
-        print("------------------")
-
-'''
-spreadData = []
-transactionData = []
-
-for i in monthlyTransactions:
-    if str(type(i)) == "<class 'transactions.CreditTransaction'>":
-        transactionData = [i.description, "Crédito",  i.amount, str(i.date)]
-    else:
-        transactionData = ["[" + i.typename + "] " + i.title, "Débito", i.detail, str(i.date)]
-    spreadData += [transactionData]
-    print(transactionData)
-
-nRows = len(spreadData)
-spreadRange = "A1:D" + str(nRows)
-print(spreadRange)
-
-#wks.update(spreadRange, spreadData)'''
