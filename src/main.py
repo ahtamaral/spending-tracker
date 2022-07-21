c8da084fc851922d18ab11eaf11cc3c845a224c1
Htutu87 <artur.ht.amaral@gmail.com>
2022-07-21 17:40:24 -0300

    Criando repositorio data automaticamente.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 
 import fetcher as fetcher
+import os
 
 # Global variables
 version = 0.1
 
 if __name__ == "__main__":
+
+    os.system("mkdir -p ../data")
 
     print("-----------------------\n Nubank Spending Tracker v{}".format(version))
     print("\n\tAutomatically monitor your credit and debit card spending on your Nubank account.")
